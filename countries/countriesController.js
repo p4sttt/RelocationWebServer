76e0d53bb67611928a5ac116830bbdfaed32396f
da_m1r <damirr765@gmail.com>
2023-01-03 13:19:29 +0300

    v3

--- countries/countriesController.js
+++ countries/countriesController.js
@@ -1,4 +1,10 @@
 const Country = require("../models/Country");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const getIdFromAcessToken = (token) => {
+  return jwt.decode(token);
+};
 
 class countriesRouter {
   async getCountries(req, res) {
@@ -25,6 +31,24 @@ class countriesRouter {
       res.status(200).json({ msg: "success" });
     } catch (error) {
       res.status(200).json({ msg: "set countries error" });
+    }
+  }
+  async getUserCountries(req, res) {
+    try {
+      const { token } = req.headers;
+      const { id } = getIdFromAcessToken(token);
+      const user = await User.findById(id);
+      const countries = user.settings.countries
+      const userCountries = [];
+
+      for(const country of countries){
+        userCountries.push(await Country.findOne({"name": country}))
+      }
+
+      res.status(200).json({ userCountries });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ msg: "get countries error" });
     }
   }
 }
--- countries/countriesRouter.js
+++ countries/countriesRouter.js
@@ -4,6 +4,7 @@ const controller = require("./countriesController");
 const router = Router();
 
 router.get("/countries", controller.getCountries);
+router.get("/user/countries", controller.getUserCountries)
 router.post("/country", controller.setCountry);
 
 module.exports = router;
